e8e94de95052200bee057cd6afac20ff25bf3dbc
Laura Pérez Mayos <lpmayos@gmail.com>
2015-02-24 23:50:45 +0100

    multilingual support and english and spanish translations

--- js/template.js
+++ js/template.js
@@ -54,21 +54,31 @@
 
         //i18next - Multilingual support
         //-----------------------------------------------
-        i18n.init(function(t) {
-            // translate nav
-            $(".nav").i18n();
+
+        i18n.init({
+            fallbackLng: false,     // fallback quando não definir linguagem
+            debug: true,            // debug do plugin
+            fixLng: true,           // preserva o cookie quando a linguagem for definida
+            load: 'current'         // define a forma correta de declarar linguagens
+        },
+          
+        function(translation) {
+            $('[data-i18n]').i18n();
+            var appName = translation('app.name');
+        });
+        
+        $('#spanish').on('click', function() {
+            i18n.setLng('es', {fixLng: true}, function(translation){
+                $('[data-i18n]').i18n();
+            });
         });
 
-        i18n.setLng("en");
+        $('#english').on('click', function() {
+            i18n.setLng('en', {fixLng: true}, function(translation){
+                $('[data-i18n]').i18n();
+            });
+        });
 
-        $("#spanish").click(function() {
-            i18n.setLng("es");
-            location.reload();
-        });
-        $("#english").click(function() {
-            i18n.setLng("en");
-            location.reload();
-        });
 
     });
 })(this.jQuery);
